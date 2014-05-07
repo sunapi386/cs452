65bb246c3ccf754cce2c68453d65a2e9cd45d0d3
Jason Sun UW <sunapi386@gmail.com>
2014-05-07 18:51:28 -0400

    timer runs\!

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,28 +7,37 @@
 
 #include <bwio.h>
 #include <ts7200.h>
+#include <kernel.h>
+#include <timer.h>
 
 // Prints a menu and returns user's choice. Returns -1 if bad choice.
-int getusrcmd( int channel ) {
-	char menu[] = "Menu.
-	1. Train track state
-	2. Clock
-	3. Hello world
-	Enter a number:\n\r";
-	bwputstr( channel, menu );
-	menu[0] = bwgetc( channel );
-	// bwprintf( channel, "You entered '%s'\n\r", menu[0] );
-	int ret = atoi( menu[0] );
-	return (ret > 0 && ret < 4)? ret : -1;
-}
+// char getusrcmd( int channel ) {
+//     char menu[] = "Menu.\n\r1. Train track state\n\r2. Clock\n\r3. Hello world\n\rEnter a number:\n\r";
+//     bwputstr( channel, menu );
+//     menu[0] = bwgetc( channel );
+//     bwprintf( channel, "You entered '%s'\n\r", menu[0] );
+//     return menu[0];
+// }
 
 int main( int argc, char* argv[] ) {
-	char str[] = "Jason Train Menu.\n\r";
-	bwsetfifo( COM2, OFF );
-	bwputstr( COM2, str );
-	int choice = getusrcmd( COM2 );
-	// bwprintf( COM2, "%s", str );
-	bwprintf( COM2, "You entered '%s'\n\r", menu[0] );
-	return 0;
+    bwsetfifo( COM2, OFF );
+    int *timer1load = (int *)(TIMER1_LOAD);
+    int *timer1val = (int *)(TIMER1_VALUE);
+    int *timer1ctl = (int *)(TIMER1_CONTROL);
+
+    /* Load contains initial values of timer */
+    *timer1load = 255;
+
+    /* Timer control: enable timer (p. 430) */
+    *timer1ctl = ENABLE_MASK;
+
+    /* Read base + 0x4 */
+
+    for( ;; ) {
+        if( *timer1val == 0 ) break;
+        bwprintf( COM2, "T1 %u\n\r", *timer1val);
+    }
+
+    return 0;
 }
 
