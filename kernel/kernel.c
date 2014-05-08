92c30864407487856cc9811abe6e48f65cbd71f9
Jason Sun UW <sunapi386@gmail.com>
2014-05-08 02:16:43 -0400

    [timer] prints nicely and ticks good

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,17 +5,33 @@
  * slow down or lose ticks
  */
 
-#include <termio.h>
+
+#include <bwio.h>
 #include <ts7200.h>
 #include <kernel.h>
 #include <timer.h>
 
 int main( int argc, char* argv[] ) {
-    termsetfifo( COM2, OFF );
+    bwsetfifo( COM2, OFF );
+    setTimer( 3, 0 );
+    volatile unsigned int msec = 0;
+    volatile unsigned int sec = 0;
+    volatile unsigned int last_time = 0;
+    volatile unsigned int now;
 
+    bwprintf( COM2, "%c[2J", 27);
     for( ;; ) {
-        termcheckandsend( COM1 );
-        termcheckandsend( COM2 );
+        now = getTimer(3) / 2;
+        if( now != last_time ) {
+            last_time = now;
+            msec += 1;
+        }
+        if( msec % 100 == 0 ) {
+            bwprintf( COM2, "%c[%d;%dH", 27, 0, 0);
+            bwprintf( COM2, "%u:%u.%u\n\r", sec / 60, sec % 60, msec );
+            if( msec % 1000 == 0 ) { msec = 0; sec += 1; }
+        }
+
     }
 
     return 0;
