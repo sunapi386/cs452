eb226d824e729969b7ece865b3c85da8906c236d
Jason Sun <sunapi386@gmail.com>
2015-05-08 20:29:35 -0400

    menu parses buffer digit correct

--- src/menu.c
+++ src/menu.c
@@ -81,9 +81,64 @@ static void menuHistory3( buffer_t *b1, buffer_t *b2, buffer_t *b3 ) {
 // 	}
 // 	POS( MENU_INPUT_ROW, MENU_INPUT_COL ); /* Park the cursor */
 // }
+int c2d( char ch ) {
+	if( ch >= '0' && ch <= '9' ) return ch - '0';
+	if( ch >= 'a' && ch <= 'f' ) return ch - 'a' + 10;
+	if( ch >= 'A' && ch <= 'F' ) return ch - 'A' + 10;
+	return -1;
+}
+
+int a2i( char *b ) {
+	int num, digit;
+	num = 0;
+	while( ( digit = c2d( *b++ ) ) >= 0 ) {
+		if ( digit > 10 ) break;
+		num = num*10 + digit;
+	}
+	return num;
+}
+
+
 
 void menuInput( buffer_t *b1, buffer_t *b2, buffer_t *b3 ) {
 	menuHistory3( b1, b2, b3 );
+	int i1, i2, i3;
+	i1 = a2i( b1->elements ); i2 = a2i( b2->elements ); i3 = a2i( b3->elements );
+
+	PRINT( " parsed [%d:%d:%d]", i1, i2, i3 );
+	// 	if( buffer[0] == 't' && buffer[1] == 'r' ) {
+	// 	int train_number, train_speed;
+	// 	/* Handles tr train_number train_speed */
+	//     CLLINE;
+	//     POS( MENU_INPUT_ROW - 1, MENU_INPUT_COL );
+	//     CLLINE;
+	//     PRINT( "tr %d %d", train_number, train_speed );
+	//     POS( MENU_INPUT_ROW, MENU_INPUT_COL );
+	//     termprintf( COM1, "%c", 97 );
+	// }
+	// else
+	// if( buffer[0] == 'r' && buffer[1] == 'v' ) {
+	// 	int train_number;
+	// 	/* Handles rv train_number */
+	//     CLLINE;
+	//     POS( MENU_INPUT_ROW - 1, MENU_INPUT_COL );
+	//     CLLINE;
+	//     PRINT( "rv %d", train_number );
+	//     POS( MENU_INPUT_ROW, MENU_INPUT_COL );
+	//     termprintf( COM1, "S50" );
+	// }
+	// else
+	// if( buffer[0] == 's' && buffer[1] == 'w' ) {
+	// 	int switch_number, switch_direction;
+	// 	/* Handles sw switch_number switch_direction */
+	// 	// a2i( , &buffer );
+	//     CLLINE;
+	//     POS( MENU_INPUT_ROW - 1, MENU_INPUT_COL );
+	//     CLLINE;
+	//     PRINT( "sw %d %c", switch_number, switch_direction );
+	//     POS( MENU_INPUT_ROW, MENU_INPUT_COL );
+	//     termprintf( COM1, "%c", 96 );
+	// }
 	POS( MENU_INPUT_ROW, MENU_INPUT_COL ); /* Park the cursor */
 }
 
